92aff538bbacab7bf9c97109a53afe0c77934123
Jakub Skowronski <jakub.skowronski@protonmail.com>
2023-10-26 23:24:17 +0200

    Setting up build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir $PREFIX/build && cd $PREFIX/build 
 cmake ../AZURE2/ -DBUILD_GUI=ON && make -j4
 
-cp src/AZURE2 "{{ PREFIX }}/lib"
+cp $PREFIX/src/AZURE2 "{{ PREFIX }}/lib"
 
 $PYTHON setup.py install
