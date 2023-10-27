95fae1707f0ab1c8fcc440ff0c6a8b0db2fc41b2
Jakub Skowronski <jakub.skowronski@protonmail.com>
2023-10-27 14:27:05 +0200

    Cleaned AZURE2 source code

--- AZURE2/src/DoubleFactorial.cpp
+++
@@ -1,8 +0,0 @@
-double DoubleFactorial(int a) {
-  double b=1;
-  while(a>1) {
-    b=b*a;
-    a-=2;
-  }
-  return b;
-}
--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
-mkdir build && cd build 
-cmake ../AZURE2/ -DBUILD_GUI=ON -DUSE_QWT=ON && make -j4 && cd -
-
-mkdir -p $PREFIX/bin
-cp build/src/AZURE2 $PREFIX/bin/
-
 python setup.py build
 python setup.py install
