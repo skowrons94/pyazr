30129b3772c90ba55d32fc092bff084566e0b34c
Jakub Skowronski <jakub.skowronski@protonmail.com>
2023-10-30 10:58:29 +0100

    Set up the module names

--- src/pyazr/__init__.py
+++ src/pyazr/__init__.py
@@ -1,1 +1,9 @@
+from .brick import brick
+from .api import api
+from .mcmc import mcmc
+from .minuit import minuit
+from .thread import thread
+from .calc import calc
+from .minimizer import minimizer
+
 __version__ = '1.0.0'
